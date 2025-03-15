f89ea8dc0ba1a311a8edf5149976f50b1f0452be
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-14 22:31:56 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string leet;
+Console.Clear();
+Console.Write("Escreva sua frase e deixe seu jeito ninja:");
+leet = Console.ReadLine()!
+.ToUpper()
+.Replace("A", "4")
+.Replace("E", "3")
+.Replace("I", "1")
+.Replace("L", "1")
+.Replace("O", "0")
+.Replace("T", "7")
+.Replace("S", "5")
+;
+
+Console.WriteLine(@$"
+{leet}");
+
